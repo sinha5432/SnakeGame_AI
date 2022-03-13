73e9b07f952a207ecb57193e55343e7c35fe876b
Manasvi Sinha <manasvishubh@gmail.com>
2022-03-13 23:36:16 +0530

    Minor tweaks

--- agent.py
+++ agent.py
@@ -140,6 +140,7 @@ def train():
 
             if score > record:
                 record = score
+                print('Saving New Model')
                 agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record: ', record)
